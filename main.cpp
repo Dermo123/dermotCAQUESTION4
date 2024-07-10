d57106e8b3a7ae695b3cd697220f091a404dd4d7
Dermo123 <dermo_oz@yahoo.com>
2024-07-10 19:58:30 +0000

    WORKS

---
+++ main.cpp
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+
+int main() {
+    // Initialize int variables
+    int a = 10;
+    int b = 20;
+    int c = 30;
+
+    // Add values of a and b, store it in a new int variable d and print value of variable d
+    int d = a + b;
+    printf("The value of variable d is: %d\n", d);
+
+    // Subtract variable c from d and store in new int variable e and print the value of variable e
+    int e = d - c;
+    printf("The value of variable e is: %d\n", e);
+
+    return 0;
+}
+
